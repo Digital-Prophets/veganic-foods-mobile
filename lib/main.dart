a93e595cc6efaf0f3311d60bb5378adba46255cd
Chriford Siame <siamechrif@gmail.com>
2022-03-31 17:03:07 +0200

    made a change

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-
 // ignore_for_file: deprecated_member_use, prefer_final_fields
 
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -32,7 +34,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   // ignore: non_constant_identifier_names
-  List<int> _totalAmmount = [];
+  String _totalAmmount = '0';
   List<dynamic> _food_objects = [
     {
       'id': 1,
@@ -58,12 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
       'image': 'rice_w_n.jpeg',
       'price': 34
     },
-    {
-      'id': 4,
-      'food': 'Rice with chilli',
-      'image': 'rice_w_c.jpg',
-      'price': 33
-    },
+    {'id': 4, 'food': 'Rice with chilli', 'image': 'rice_w_c.jpg', 'price': 33},
     {
       'id': 5,
       'food': 'Rice with tomato source',
@@ -77,82 +74,99 @@ class _MyHomePageState extends State<MyHomePage> {
       'price': 34
     },
   ];
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-      child: Column(
-        children: [
-          // 1. for image in header
-          FittedBox(
-          fit: BoxFit.fitWidth,
-          child: Image.asset(
-            'assets/images/bg_image2.jpg'
-          ,)
-          ),
-          // for food listing
-          Container(
-            alignment: FractionalOffset.center,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                BackButton(
-                onPressed: () {  },
-                ),
-                const Text('My Basket', style: TextStyle(color: Color(0xFF2E3233), fontWeight: FontWeight.bold, fontSize: 24),),
-                const Text('K176.00', style: TextStyle(color: Color(0xFF2E3233), fontWeight: FontWeight.bold, fontSize: 24),),
-              ],
-            ),
-          ),
-
-          Column(
-            children: _food_objects.map((element) =>
-            Card(
-            child: Row(
-              children: [
-                Padding(
-                padding: const EdgeInsets.all(6.0),
-                child: Container(
-                    decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    color: Color.fromARGB(255, 49, 45, 45),
+        body: SingleChildScrollView(
+            child: Column(
+      children: [
+        Column(
+          children: [
+            // 1. for image in header
+            FittedBox(
+                fit: BoxFit.fitWidth,
+                child: Image.asset(
+                  'assets/images/bg_image2.jpg',
+                )),
+            // for food listing
+            Container(
+              alignment: FractionalOffset.centerLeft,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  TextButton( onPressed: () {
+                    
+                  }, child: const Icon(
+                    Icons.arrow_back_ios_rounded, color: Color.fromRGBO(0, 0, 0, 0.8)
                     ),
-                    child: Image.asset("assets/images/${element['image']}", width: 70, height: 70,),
                   ),
-                ),
-                Column(
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: Text(
-                        element['food'].toString(),
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold)
-                      ),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Text(
-                        "K"+element['price'].toString(),
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold)
-                      ),
-                    ),
-                  ],
-                ),
-               
-              ],
+                  const Text(
+                    'My Basket',
+                    style: TextStyle(
+                        color: Color(0xFF2E3233),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24),
+                  ),
+                ],
+              ),
             ),
-          )).toList()
+
+            Column(
+                children: _food_objects
+                    .map((element) => Card(
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(6.0),
+                                child: Container(
+                                  decoration: const BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10.0)),
+                                    color: Color.fromARGB(255, 49, 45, 45),
+                                  ),
+                                  child: Image.asset(
+                                    "assets/images/${element['image']}",
+                                    width: 70,
+                                    height: 70,
+                                  ),
+                                ),
+                              ),
+                              Column(
+                                children: [
+                                  Text(element['food'].toString(),
+                                      style: const TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold)),
+                                  Text("K" + element['price'].toString(),
+                                      style: const TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold)),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ))
+                    .toList()),
+          ],
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 12.0, bottom: 12.0),
+          child: ElevatedButton(
+            onPressed: () {
+              // ignore: avoid_print
+              setState(() {
+                _food_objects.map((e) => _totalAmmount + e['price']);
+                _food_objects[0]["food"] = _totalAmmount;
+              });
+            },
+            child: const Text(
+              'Complete Order',
+              style: TextStyle(fontSize: 30),
+            ),
           ),
-
-          
-        ],
-
-      ))
-    );
+        )
+      ],
+    )));
   }
 }
